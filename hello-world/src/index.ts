823e63ef8651e68e5becf53e22cda4c7333a4162
Wade Desir <wededesir@gmail.com>
2023-02-03 20:05:01 -0500

    starting generics

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -102,32 +102,36 @@
 
 
 //////////Exercises for: Classes, Interfaces, and object-oriented programming
-class Logger{
-    constructor(public fileName: string){
+// class Logger{
+//     constructor(public fileName: string){
 
-    }
+//     }
 
-    writeToFile(){
-        console.log('writing to logfile for: ' + this.fileName)
-    }
-}
+//     writeToFile(){
+//         console.log('writing to logfile for: ' + this.fileName)
+//     }
+// }
 
-class Person {
-    constructor(public firstName: string, public lastName: string){}
+// class Person {
+//     constructor(public firstName: string, public lastName: string){}
 
-    get fullName(){
-        return this.firstName + ' ' + this.lastName
-    }
-}
+//     get fullName(){
+//         return this.firstName + ' ' + this.lastName
+//     }
+// }
 
-class Employee extends Person {
-    constructor(public salary: number, firstName: string, lastName: string){
-        super(firstName, lastName)
-    }
-}
+// class Employee extends Person {
+//     constructor(public salary: number, firstName: string, lastName: string){
+//         super(firstName, lastName)
+//     }
+// }
 
 // The difference between private and protected members is that private cannot be inherited while protected members can but just can't be modified
+// madistrict27
 
+/*
+
+*/
 //Given the data below, define an interface for representing employees: -got this wrong
 // interface Employee {
 //     name: string,
@@ -141,17 +145,17 @@ class Employee extends Person {
 
 // right answer: 
 
-interface Address {
-    street: string,
-    city: string,
-    zipCode: number
-}
+// interface Address {
+//     street: string,
+//     city: string,
+//     zipCode: number
+// }
 
-interface Emplyee {
-    name: string,
-    salary: string,
-    address: Address
-}
+// interface Emplyee {
+//     name: string,
+//     salary: string,
+//     address: Address
+// }
 
 
 
